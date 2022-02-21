d1437573f2da393a3fb5731266c56ba8baf80b97
RaulNavarroGil <98021131+RaulNavarroGil@users.noreply.github.com>
2022-02-21 12:11:11 +0100

    Examen

---
+++ Examen de Entorno/src/Ejercicios/Ejercicio5.java
@@ -0,0 +1,20 @@
+package Ejercicios;
+
+/**
+ * 
+ * @author Raul
+ * @version 1.0
+ * @since 21-02-2022
+ * @see <a href:"https://github.com/RaulNavarroGil/ExamenEEDDT4-RaulNavarro">
+ *
+ */
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+		
+	}
+
+}
---
+++ Examen de Entorno/src/Ejercicios/Ejercicio7.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Prueba para el ejercicio 7");
+		
+	}
+
+}
